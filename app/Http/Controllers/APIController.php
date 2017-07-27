1414c9857bb8e6b6feecea821c6d55d6cb55d9ce
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2017-07-27 16:15:16 -0400

    add some basic API methods to fetch MFL data

---
+++ app/Http/Controllers/APIController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Carbon\Carbon;
+
+class APIController extends Controller
+{
+    public function league() {
+        $cacheTime = Carbon::now()->addMinutes(10);
+
+        $host = Cache::remember('mfl_league_host', $cacheTime, function() {
+            return config('mfl.league_host');
+        });
+
+        $year = Cache::remember('mfl_league_year', $cacheTime, function() {
+            return config('mfl.league_year');
+        });
+
+
+        $client = new Client([
+            'base_uri' => "https://$host/$year/"
+        ]);
+
+        $data = $client->get('export', ['query' => [
+            'L' => config('mfl.league_id'),
+            'APIKEY' => config('mfl.api_key'),
+            'JSON' => 1,
+            'TYPE' => 'league'
+        ]])->getBody()->getContents();
+
+        return response($data)->header('Content-Type', 'application/json');
+
+    }
+
+    public function standings() {
+        $cacheTime = Carbon::now()->addMinutes(10);
+
+        $host = Cache::remember('mfl_league_host', $cacheTime, function() {
+            return config('mfl.league_host');
+        });
+
+        $year = Cache::remember('mfl_league_year', $cacheTime, function() {
+            return config('mfl.league_year');
+        });
+
+
+        $client = new Client([
+            'base_uri' => "https://$host/$year/"
+        ]);
+
+        $data = $client->get('export', ['query' => [
+            'L' => config('mfl.league_id'),
+            'APIKEY' => config('mfl.api_key'),
+            'JSON' => 1,
+            'TYPE' => 'leagueStandings'
+        ]])->getBody()->getContents();
+
+        return response($data)->header('Content-Type', 'application/json');
+    }
+}
