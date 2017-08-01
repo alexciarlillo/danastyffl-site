fb0de8ec245bb3db3c6e319d0a274b2020ec7b82
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2017-07-31 21:34:45 -0400

    create a true header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,25 +23,10 @@
     <body>
         <div class="hero-banner"></div>
         <div class="hero-cover"></div>
+        
+        @include('layouts.header')
+
         <div class="container content">
-            <div class="row">
-                <div class="col-12 col-md-10 offset-md-1">
-
-                    <div class="row">
-                        <div class="col-12">
-                            <h1 class="title text-center">DaNasty FFL</h1>
-                        </div>
-                    </div>
-
-                    <div class="row mt-4 mb-4">
-                        <div class="col-10 offset-1 col-md-6 offset-md-3">
-                            <a class="btn btn-secondary btn-block" href="https://{{ config('mfl.league_host') }}/2017/home/{{ config('mfl.league_id')}}">Go to MFL</a>
-                            <a class="btn btn-secondary btn-block" href="/login">Login</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
             <div class="row mt-2">
                 <div class="col-12 col-md-10 offset-md-1">
                     @yield('content')
